c59a832d06aecbd5f09615ab2428b4d7d2ea566f
SCollins <sje.collins@gmail.com>
2022-10-19 11:26:19 +0000

    Fin checkout

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,26 +2,30 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
-class OrderLineItemsAdminInline(admin.TabularInline):
+class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
-    readonly_fields = ('lineitem_total', )
+    readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
-    inlines = (OrderLineItemsAdminInline, )
+    inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'date', 'delivery_cost',
-                       'order_total', 'grand_total', )
-    
-    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number',
-              'country', 'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county', 'delivery_cost', 'order_total',
-              'grand_total',)
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
-    list_display = ('order_number', 'date', 'full_name', 'order_total',
-                    'delivery_cost', 'grand_total',)
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid')
 
-    ordering = ('-date', )
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
 
+    ordering = ('-date',)
 
 admin.site.register(Order, OrderAdmin)
