eb2a3fa2e785f66cba3adda8a002958b05353990
SCollins <sje.collins@gmail.com>
2022-10-22 18:44:19 +0000

    Fin profiles

--- home/views.py
+++ home/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def index(request):
-    """
-    View to return index page
-    """
+    """ A view to return the index page """
+
     return render(request, 'home/index.html')
--- products/admin.py
+++ products/admin.py
@@ -1,20 +1,28 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Product, Category
 
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = (
-        'friendly_name', 'name',
-    )
+# Register your models here.
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
-        'sku', 'name', 'category', 'price', 'rating', 'image',
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
     )
 
-    ordering = ('sku', )
+    ordering = ('sku',)
 
 
-admin.site.register(Category, CategoryAdmin)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
